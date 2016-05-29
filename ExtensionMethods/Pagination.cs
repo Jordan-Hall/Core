bf52920a4acdab42d1c95644c3a8f17e6d38aa2e
Jordan Hall <Jordan@JordanHall.me>
2016-05-29 22:49:19 +0100

    Added Pagination linq extension method

---
+++ ExtensionMethods/Pagination.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class Pagination
+    {
+        public static IEnumerable<T> Page<T>(this IEnumerable<T> source, int pageIndex = 1, int pageSize = 20)
+        {
+            return source.Skip((pageIndex - 1)*pageSize).Take(pageSize);
+        }
+        public static IQueryable<T> Page<T>(this IQueryable<T> source, int pageIndex = 1, int pageSize = 20)
+        {
+            return source.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
